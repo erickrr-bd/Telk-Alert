d8877cddbc4992190509e81ad7a21499a142b039
Erick Rodriguez <erickrr.tbd93@gmail.com>
2022-06-28 15:58:21 -0500

    Telk-Alert-Report: Version 3.2

--- Telk-Alert-Suite/Telk-Alert-Report/modules/Telk_Alert_Report_Class.py
+++ Telk-Alert-Suite/Telk-Alert-Report/modules/Telk_Alert_Report_Class.py
@@ -1,10 +1,73 @@
-
+from threading import Thread
+from libPyLog import libPyLog
+from libPyElk import libPyElk
+from libPyUtils import libPyUtils
+from .Constants_Class import Constants
 
 class TelkAlertReport:
 
+	__utils = None
+
+	__logger = None
+
+	__constants = None
+
+	__elasticsearch = None
+
 
 	def __init__(self):
-		print("Hola")
+		self.__logger = libPyLog()
+		self.__utils = libPyUtils()
+		self.__constants = Constants()
+		self.__elasticsearch = libPyElk()
+
 
 	def startTelkAlertReport(self):
-		print("Hola2")
+		try:
+			data_report_configuration = self.__utils.readYamlFile(self.__constants.PATH_FILE_REPORT_CONFIGURATION)
+			data_configuration = self.__utils.readYamlFile(self.__constants.PATH_FILE_CONFIGURATION)
+			if data_configuration["use_http_authentication"] == True:
+					conn_es = self.__elasticsearch.createConnectionToElasticSearch(data_configuration, path_key_file = self.__constants.PATH_KEY_FILE)
+			else:
+				conn_es = self.__elasticsearch.createConnectionToElasticSearch(data_configuration)
+			if not conn_es == None:
+				self.__logger.generateApplicationLog("Telk-Alert-Report v3.2", 1, "__start", use_stream_handler = True)
+				self.__logger.generateApplicationLog("@2022 Tekium. All rights reserved.", 1, "__start", use_stream_handler = True)
+				self.__logger.generateApplicationLog("Author: Erick Rodriguez", 1, "__start", use_stream_handler = True)
+				self.__logger.generateApplicationLog("Email: erodriguez@tekium.mx, erickrr.tbd93@gmail.com", 1, "__start", use_stream_handler = True)
+				self.__logger.generateApplicationLog("License: GPLv3", 1, "__start", use_stream_handler = True)
+				self.__logger.generateApplicationLog("Telk-Alert-Report started", 1, "__start", use_stream_handler = True)
+				self.__logger.generateApplicationLog("Established connection with: " + data_configuration['es_host'] + ':' + str(data_configuration['es_port']), 1, "__start" , use_stream_handler = True)
+				self.__logger.generateApplicationLog("Elasticsearch Cluster Name: " + conn_es.info()["cluster_name"], 1, "__start", use_stream_handler = True)
+				self.__logger.generateApplicationLog("Elasticsearch Version: " + conn_es.info()["version"]["number"], 1, "__start", use_stream_handler = True)
+				path_alert_rules_folder = self.__constants.PATH_BASE_TELK_ALERT + '/' + data_configuration["name_folder_rules"]
+				list_all_alert_rules = self.__utils.getListOfAllYamlFilesInFolder(path_alert_rules_folder)
+				if list_all_alert_rules:
+					self.__logger.generateApplicationLog(str(len(list_all_alert_rules)) + " alert rules in: " + path_alert_rules_folder, 1, "__start", use_stream_handler = True)
+					for alert_rule in list_all_alert_rules:
+						self.__logger.generateApplicationLog(alert_rule[:-5] + " loaded", 1, "__start", use_stream_handler = True)
+						data_alert_rule = self.__utils.readYamlFile(path_alert_rules_folder + '/' + alert_rule)
+						Thread(name = alert_rule[:-5], target = self.__getAlertRuleReport, args = (conn_es, data_alert_rule, )).start()
+				else:
+					self.__logger.generateApplicationLog("No alert rules found in: " + path_alert_rules_folder, 1, "__start", use_stream_handler = True)
+		except KeyError as exception:
+			print("Error")
+
+
+	def __getAlertRuleReport(self, conn_es, data_alert_rule):
+		try:
+			search_in_elastic = self.__elasticsearch.createSearchObject(conn_es, data_alert_rule["index_pattern_name"])
+			query_string_alert_rule = data_alert_rule["query_type"][0]["query_string"]["query"]
+			if not data_alert_rule["use_custom_rule_option"] == True:
+				if data_alert_rule["use_fields_option"] == True:
+					result_search = self.__elasticsearch.executeSearchQueryString(search_in_elastic, "now-1d/d", "now/d", query_string_alert_rule, data_alert_rule["use_fields_option"], fields = data_alert_rule["fields_name"])
+				else:
+					result_search = self.__elasticsearch.executeSearchQueryString(search_in_elastic, "now-1d/d", "now/d", query_string_alert_rule, data_alert_rule["use_fields_option"])
+				if result_search:
+					if len(result_search) >= data_alert_rule["number_events_found_by_alert"]:
+						for hit in result_search:
+							print(hit)
+				else:
+					self.__logger.generateApplicationLog("No events found", 1, "__" + data_alert_rule["alert_rule_name"], use_stream_handler = True)
+		except KeyError as exception:
+			print("Error")
