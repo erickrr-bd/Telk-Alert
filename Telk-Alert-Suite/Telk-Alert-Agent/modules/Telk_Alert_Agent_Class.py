d205bc219f4ca3a62f5012199dc762ea3a813ed4
Erick Rodriguez <erickrr.tbd93@gmail.com>
2023-01-24 16:10:41 -0600

    Telk-Alert-Agent: Version 3.3

--- Telk-Alert-Suite/Telk-Alert-Agent/modules/Telk_Alert_Agent_Class.py
+++ Telk-Alert-Suite/Telk-Alert-Agent/modules/Telk_Alert_Agent_Class.py
@@ -1,4 +1,4 @@
-from os import popen
+from os import popen, path
 from datetime import datetime
 from libPyLog import libPyLog
 from time import sleep,strftime
@@ -26,40 +26,42 @@ class TelkAlertAgent:
 		Method that starts the Telk-Alert-Agent application.
 		"""
 		try:
-			telk_alert_agent_data = self.__utils.readYamlFile(self.__constants.PATH_TELK_ALERT_AGENT_CONFIGURATION_FILE)
-			first_execution = telk_alert_agent_data["first_execution"].split(':')
-			second_execution = telk_alert_agent_data["second_execution"].split(':')
-			passphrase = self.__utils.getPassphraseKeyFile(self.__constants.PATH_KEY_FILE)
-			telegram_bot_token = self.__utils.decryptDataWithAES(telk_alert_agent_data["telegram_bot_token"], passphrase).decode("utf-8")
-			telegram_chat_id = self.__utils.decryptDataWithAES(telk_alert_agent_data["telegram_chat_id"], passphrase).decode("utf-8")
 			self.__logger.generateApplicationLog("Telk-Alert-Agent v3.3", 1, "__start", use_stream_handler = True)
 			self.__logger.generateApplicationLog("@2023 Tekium. All rights reserved.", 1, "__start", use_stream_handler = True)
 			self.__logger.generateApplicationLog("Author: Erick Rodriguez", 1, "__start", use_stream_handler = True)
 			self.__logger.generateApplicationLog("Email: erodriguez@tekium.mx, erickrr.tbd93@gmail.com", 1, "__start", use_stream_handler = True)
 			self.__logger.generateApplicationLog("License: GPLv3", 1, "__start", use_stream_handler = True)
 			self.__logger.generateApplicationLog("Telk-Alert-Agent started", 1, "__start", use_stream_handler = True)
-			while True:
-				telk_alert_service_command = popen('(systemctl is-active --quiet telk-alert.service && echo "Running" || echo "Not running")')
-				result_command_lines = telk_alert_service_command.readlines()
-				for line in result_command_lines:
-					telk_alert_service_status = line.rstrip('\n')
-				if telk_alert_service_status == "Not running":
-					message_telegram = self.__generateTelegramMessage(telk_alert_service_status)
-					response_http_code = self.__telegram.sendMessageTelegram(telegram_bot_token, telegram_chat_id, message_telegram)
-					self.__createLogByTelegramCode(response_http_code)
-					self.__logger.generateApplicationLog("Status: " + telk_alert_service_status, 2, "__serviceStatus", use_file_handler = True, name_file_log = self.__constants.NAME_FILE_LOG, user = self.__constants.USER, group = self.__constants.GROUP)
-				else:
-					now = datetime.now()
-					if(now.hour == int(first_execution[0]) and now.minute == int(first_execution[1])) or (now.hour == int(second_execution[0]) and now.minute == int(second_execution[1])):
+			if path.exists(self.__constants.PATH_TELK_ALERT_AGENT_CONFIGURATION_FILE):
+				self.__logger.generateApplicationLog("Configuration file found in: " + self.__constants.PATH_TELK_ALERT_AGENT_CONFIGURATION_FILE, 1, "__readConfigurationFile", use_stream_handler = True)
+				telk_alert_agent_data = self.__utils.readYamlFile(self.__constants.PATH_TELK_ALERT_AGENT_CONFIGURATION_FILE)
+				first_execution = telk_alert_agent_data["first_execution"].split(':')
+				second_execution = telk_alert_agent_data["second_execution"].split(':')
+				passphrase = self.__utils.getPassphraseKeyFile(self.__constants.PATH_KEY_FILE)
+				telegram_bot_token = self.__utils.decryptDataWithAES(telk_alert_agent_data["telegram_bot_token"], passphrase).decode("utf-8")
+				telegram_chat_id = self.__utils.decryptDataWithAES(telk_alert_agent_data["telegram_chat_id"], passphrase).decode("utf-8")
+				while True:
+					telk_alert_service_command = popen('(systemctl is-active --quiet telk-alert.service && echo "Running" || echo "Not running")')
+					result_command_lines = telk_alert_service_command.readlines()
+					for line in result_command_lines:
+						telk_alert_service_status = line.rstrip('\n')
+					if telk_alert_service_status == "Not running":
 						message_telegram = self.__generateTelegramMessage(telk_alert_service_status)
 						response_http_code = self.__telegram.sendMessageTelegram(telegram_bot_token, telegram_chat_id, message_telegram)
 						self.__createLogByTelegramCode(response_http_code)
-				self.__logger.generateApplicationLog("Status: " + telk_alert_service_status, 1, "__serviceStatus", use_stream_handler = True)
-				sleep(60)
+						self.__logger.generateApplicationLog("Status: " + telk_alert_service_status, 2, "__serviceStatus", use_file_handler = True, name_file_log = self.__constants.NAME_FILE_LOG, user = self.__constants.USER, group = self.__constants.GROUP)
+					else:
+						now = datetime.now()
+						if(now.hour == int(first_execution[0]) and now.minute == int(first_execution[1])) or (now.hour == int(second_execution[0]) and now.minute == int(second_execution[1])):
+							message_telegram = self.__generateTelegramMessage(telk_alert_service_status)
+							response_http_code = self.__telegram.sendMessageTelegram(telegram_bot_token, telegram_chat_id, message_telegram)
+							self.__createLogByTelegramCode(response_http_code)
+					self.__logger.generateApplicationLog("Status: " + telk_alert_service_status, 1, "__serviceStatus", use_stream_handler = True)
+					sleep(60)
 		except KeyError as exception:
 			self.__logger.generateApplicationLog("Key Error: " + str(exception), 3, "__start", use_stream_handler = True, use_file_handler = True, name_file_log = self.__constants.NAME_FILE_LOG, user = self.__constants.USER, group = self.__constants.GROUP)
 		except (OSError, FileNotFoundError) as exception:
-			self.__logger.generateApplicationLog("File Not Found or OS Error. For more information, see the logs.", 3, "__readConfiguration", use_stream_handler = True)
+			self.__logger.generateApplicationLog("File Not Found or OS Error. For more information, see the logs.", 3, "__start", use_stream_handler = True)
 			self.__logger.generateApplicationLog(exception, 3, "__start", use_stream_handler = True, use_file_handler = True, name_file_log = self.__constants.NAME_FILE_LOG, user = self.__constants.USER, group = self.__constants.GROUP)
 
 
