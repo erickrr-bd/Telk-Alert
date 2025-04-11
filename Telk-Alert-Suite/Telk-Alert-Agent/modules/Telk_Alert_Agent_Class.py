d121b1051b6b022e0121677eab29ba1794c7dee2
Erick Rodriguez <erickrr.tbd93@gmail.com>
2025-04-11 13:16:41 -0600

    Fixes and new features

--- Telk-Alert-Suite/Telk-Alert-Agent/modules/Telk_Alert_Agent_Class.py
+++ Telk-Alert-Suite/Telk-Alert-Agent/modules/Telk_Alert_Agent_Class.py
@@ -26,7 +26,7 @@ class TelkAlertAgent:
 			self.logger.create_log("Author: Erick Roberto Rodríguez Rodríguez", 2, "_start", use_stream_handler = True)
 			self.logger.create_log("Email: erickrr.tbd93@gmail.com, erodriguez@tekium.mx", 2, "_start", use_stream_handler = True)
 			self.logger.create_log("Github: https://github.com/erickrr-bd/Telk-Alert", 2, "_start", use_stream_handler = True)
-			self.logger.create_log("Telk-Alert-Agent v4.0 - March 2025", 2, "_start", use_stream_handler = True)
+			self.logger.create_log("Telk-Alert-Agent v4.0 - April 2025", 2, "_start", use_stream_handler = True)
 			if path.exists(self.constants.TELK_ALERT_AGENT_CONFIGURATION):
 				self.logger.create_log(f"Configuration found: {self.constants.TELK_ALERT_AGENT_CONFIGURATION}", 2, "_readConfiguration", use_stream_handler = True)
 				telk_alert_agent_data = self.utils.read_yaml_file(self.constants.TELK_ALERT_AGENT_CONFIGURATION)
@@ -42,15 +42,21 @@ class TelkAlertAgent:
 						telegram_message = self.generate_telegram_message(telk_alert_status)
 						response_http_code = self.telegram.send_telegram_message(telegram_bot_token, telegram_chat_id, telegram_message)
 						self.create_log_by_telegram_code(response_http_code)
+						result = self.utils.manage_daemon("telk-alert.service", 1)
+						if result == 0:
+							self.logger.create_log("Telk-Alert service has been started", 3, "_statusService", use_stream_handler = True, use_file_handler = True, file_name = self.constants.LOG_FILE, user = self.constants.USER, group = self.constants.GROUP)
 					else:
 						telk_alert_pid = self.utils.get_pid_by_daemon("telk-alert.service")
 						telk_alert_threads = self.utils.get_threads_by_pid(telk_alert_pid)
-						alert_rules_total = len(self.utils.get_yaml_files_in_folder(self.constants.ALERT_RULES_FOLDER))
-						if not (telk_alert_threads - 1) == alert_rules_total:	
-							telegram_message = self.generate_telegram_message_threads(telk_alert_threads, alert_rules_total)
+						alert_rules = len(self.utils.get_yaml_files_in_folder(self.constants.ALERT_RULES_FOLDER))
+						if not (telk_alert_threads - 1) == alert_rules:	
+							telegram_message = self.generate_telegram_message_threads(telk_alert_threads, alert_rules)
 							response_http_code = self.telegram.send_telegram_message(telegram_bot_token, telegram_chat_id, telegram_message)
 							self.create_log_by_telegram_code(response_http_code)
-						self.logger.create_log(f"Threads: {telk_alert_threads - 1}, Alert Rules: {alert_rules_total}", "_statusService", use_stream_handler = True)
+							result = self.utils.manage_daemon("telk-alert.service", 2)
+							if result == 0:
+								self.logger.create_log(f"Telk-Alert service has been restarted because there are {str(telk_alert_threads - 1)}/{alert_rules} alert rules running", 3, "_statusService", use_stream_handler = True, use_file_handler = True, file_name = self.constants.LOG_FILE, user = self.constants.USER, group = self.constants.GROUP)
+						self.logger.create_log(f"Threads: {telk_alert_threads - 1}, Alert Rules: {alert_rules}", 2, "_statusService", use_stream_handler = True)
 					sleep(frequency_time)
 			else:
 				self.logger.create_log("Configuration not found.", 4, "_readConfiguration", use_stream_handler = True)
@@ -69,25 +75,26 @@ class TelkAlertAgent:
 		Returns:
 			telegram_message (str): Message to be sent via Telegram.
 		"""
-		telegram_message = "" + u'\u26A0\uFE0F' + " Telk-Alert Service " + u'\u26A0\uFE0F' + "\n\n" + u'\u23F0' + "Service Status Validation Time: " + strftime("%c") + "\n\n\n"
-		telegram_message += "Telk-Alert Service Status: " + u'\U0001f534' + "\n\n"
-		telegram_message += "" + u'\U0001f4cb' + " Note: The red circle indicates that Telk-Alert service isn't working. Report to an administrator.\n\n"
+		telegram_message = f"{u'\u26A0\uFE0F'} Telk-Alert Service {u'\u26A0\uFE0F'}\n\n{u'\u23F0'} Service Status Validation Time: {strftime("%c")}\n\n\n"
+		telegram_message += "Telk-Alert Service Status: 🔴\n\n"
+		telegram_message += f"{u'\U0001f4cb'} NOTE: The red circle indicates that Telk-Alert service isn't working. Report to an administrator.\n\n"
 		return telegram_message
 
 
-	def generate_telegram_message_threads(self, total_threads: int, alert_rules_total: int) -> str:
+	def generate_telegram_message_threads(self, total_threads: int, alert_rules: int) -> str:
 		"""
 		Method that generates the message to be sent via Telegram based on the number of threads and alert rules in use.
 
 		Parameters:
 			total_threads (int): Total number of threads running.
-			alert_rules_total (int): Total alert rules created.
+			alert_rules (int): Total alert rules created.
 
 		Returns:
 			telegram_message (str): Message to be sent via Telegram.
 		"""
-		telegram_message = "" + u'\u26A0\uFE0F' + " Telk-Alert Service " + u'\u26A0\uFE0F' + "\n\n" + u'\u23F0' + "Service Status Validation Time: " + strftime("%c") + "\n\n\n"
-		telegram_message += "" + u'\U0001f4cb' + " The Telk-Alert service has restarted because there are " + str(total_threads) + '/' + str(alert_rules_total) + " alert rules running.\n\n"
+		telegram_message = f"{u'\u26A0\uFE0F'} Telk-Alert Service {u'\u26A0\uFE0F'}\n\n{u'\u23F0'} Service Status Validation Time: {strftime("%c")}\n\n\n"
+		telegram_message += "Telk-Alert Service Status: 🟡\n\n"
+		telegram_message += f"{u'\U0001f4cb'} NOTE: Telk-Alert service has been restarted because there are {str(total_threads - 1)}/{alert_rules} alert rules running.\n\n"
 		return telegram_message
 
 
